635092d6085d0149bc57fe57cd329af4ef788f5b
amorrowbellarmine <46689625+amorrowbellarmine@users.noreply.github.com>
2020-12-03 10:14:10 -0500

    Day 3

---
+++ Day3/day3_1.ps1
@@ -0,0 +1,26 @@
+﻿$slope = Get-Content .\input_1.txt
+
+$width = $slope[0].ToCharArray().Count
+
+$position = New-Object -TypeName psobject -ArgumentList @{x=0;y=1}
+$offset = 0
+$trees = 0
+
+$down = 1
+$right = 3
+
+foreach ($line in $slope) {
+    $look = $line.ToCharArray()
+    if ($look[$position.x] -eq "#"){
+        $trees ++
+    }
+    $position.x += $right
+    if ($position.x -gt $width-1) {
+        $offset += $width
+        $position.x -= $width
+    }
+
+    $position.y += $down
+}
+
+Write-Host "Trees $trees"
---
+++ Day3/day3_2.ps1
@@ -0,0 +1,44 @@
+﻿$slope = Get-Content .\input_1.txt
+
+$width = $slope[0].ToCharArray().Count
+
+$position = New-Object -TypeName psobject -ArgumentList @{x=0;y=0}
+$offset = 0
+$alltrees = 1
+
+$routes = @()
+$routes += New-Object -TypeName psobject -ArgumentList @{right=1;down=1}
+$routes += New-Object -TypeName psobject -ArgumentList @{right=3;down=1}
+$routes += New-Object -TypeName psobject -ArgumentList @{right=5;down=1}
+$routes += New-Object -TypeName psobject -ArgumentList @{right=7;down=1}
+$routes += New-Object -TypeName psobject -ArgumentList @{right=1;down=2}
+
+foreach ($route in $routes) {
+    $trees = 0
+    $position.x = 0
+    $position.y = 0
+    $right = $route.right
+    $down = $route.down
+
+    do {
+        $line = $slope[$position.y]
+
+        $look = $line.ToCharArray()
+        if ($look[$position.x] -eq "#"){
+            $trees ++
+        }
+        $position.x += $right
+        if ($position.x -gt $width-1) {
+            $offset += $width
+            $position.x -= $width
+        }
+    
+        $position.y += $down
+
+    } until ($position.y -ge $slope.count)
+    
+    Write-Host "Trees $trees"
+    $alltrees *= $trees
+}
+
+Write-Host "AllTrees $alltrees"
